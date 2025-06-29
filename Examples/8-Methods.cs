51d65752e19ca4fe1670b99f15d8c569c783f5f9
DaIsimsiz <daisimsiz.tr@gmail.com>
2025-06-29 16:52:14 +0300

    Added 8-9

---
+++ Examples/8-Methods.cs
@@ -0,0 +1,56 @@
+﻿// Methods
+// This code will explain the basics of methods.
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(AddNumbers(7, 17));
+        Console.WriteLine(IsEven(43651));
+        Console.WriteLine(Reverse("The quick brown fox jumps over the lazy dog."));
+
+        foreach (string number in IntToStringArray([1, 2, 3, 4, 5]))
+        {
+            Console.WriteLine(number);
+        }
+    }
+
+    // Methods are composed of 4 main parts
+    static
+    int            // Return type
+    AddNumbers     // Method name
+    (int a, int b) // Input variables
+    {              /// Code
+        Console.WriteLine("AddNumbers method was called!");
+        return (a + b); // The "return" statement ends the code and returns the variable after it.
+    }              /// Code 
+
+    /////////////////////////////////////////
+    // Below this are some other examples! //
+    /////////////////////////////////////////
+
+    static bool IsEven(int n) // Returns true if the number is even, otherwise returns false.
+    {
+        if (n % 2 == 0) // If the remainder is 0, that means the number is a multiple of 2 (even)
+        { return true; }
+        else
+        { return false; }
+    }
+
+    static string Reverse(string text) // Reverses a string.
+    {
+        char[] reverse = text.ToCharArray();
+        Array.Reverse(reverse);
+        return new string(reverse);
+    }
+
+    static string[] IntToStringArray(int[] arr) // Converts an array of integers to an array of strings.
+    {
+        string[] returnArr = new string[arr.Length];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            returnArr[i] = arr[i].ToString();
+        }
+        return returnArr;
+    }
+}
---
+++ Examples/9-Classes, Static objects and Access modifiers.cs
@@ -0,0 +1,48 @@
+﻿// Classes, Static Objects and Access Modifiers
+// This code will explain the basics of classes.
+// Static objects and access modifiers are explained below. They are used to prevent accidental assignments / changes to important variables later.
+
+class Program
+{
+    static void Main()
+    {
+        Contact friend = new("John Smith", 24, 123456789012, false);
+        Console.WriteLine($"{friend.name}'s ({friend.age}{friend.Gender()}) number is {friend.number}.");
+        Contact.Test();
+    }
+}
+class Contact
+{
+    public string name;
+    public int age;
+    public long number;
+    bool isFemale;
+    // By default, classes are 'public' and anything inside them is 'private.'
+    // 'public' objects can be accessed from outside, while 'private' objects can't.
+    // In here, we keep 'isFemale' private so we can create a method to return 'M' or 'F' instead of 'true' or 'false.'
+
+    public Contact(string a, int b, long c, bool d) // This is constructor. This method will help us create a "Contact" variable! This isn't the only way to do this, but it is one of the better methods.
+    {
+        name = a;
+        age = b;
+        number = c;
+        isFemale = d;
+    }
+
+    public char Gender()
+    {
+        if (isFemale) { return 'F'; }
+        else { return 'M'; }
+    }
+
+    public static void Test()
+    {
+        Console.WriteLine("This is a static method!");
+    }
+    // Anything not marked as 'static' is instanced, and is per-object. The way you reference static vs instanced methods/variables is:
+    // Static =
+        // Class.variable / Class.method()
+    // Instanced =
+        // Class myClass = new();
+        // myClass.variable; / myClass.method(); 
+}
